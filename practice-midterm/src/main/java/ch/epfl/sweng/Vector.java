eb20065aa13f9d6f15d46a2c8cce29b28e34a488
Sergei Volodin <etoestja@yandex.ru>
2017-11-08 21:35:56 +0100

    Implement and test Vector

--- practice-midterm/src/main/java/ch/epfl/sweng/Vector.java
+++ practice-midterm/src/main/java/ch/epfl/sweng/Vector.java
@@ -1,30 +1,69 @@
 package ch.epfl.sweng;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+import static java.lang.Math.sqrt;
+
 /**
  * A vector of scalar values
  */
-public final class Vector {
+public final class Vector{
 
+	// number of components
+	private int _dimension = 0;
+
+	// stored values as an array
+	private ArrayList<Double> _values = null;
+
+	// construct and allocate space for each component
+	// default value is 0
 	public Vector(int dimension) {
-		// TODO
+		if(dimension < 0) {
+			throw new IllegalArgumentException("Dimension must be greater than zero");
+		}
+
+		_dimension = dimension;
+		_values = new ArrayList<Double>(Collections.nCopies(_dimension, 0.0));
 	}
 
+	// get element at index
 	public double getElement(int index) {
-		// TODO
-		return 0;
+		return _values.get(index);
 	}
 
+	// set element at index to a value
 	public void setElement(int index, double value) {
-		// TODO
+		_values.set(index, value);
 	}
 
+	// return the number of components
 	public int dimension() {
-		// TODO
-		return 0;
+		return _dimension;
 	}
 
+	// return the L2 norm of the vector
 	public double norm() {
-		// TODO
-		return 0;
+		Double sum = 0.0;
+		for(Double item: _values) {
+			sum += item * item;
+		}
+		return sqrt(sum);
+	}
+
+	// set L2 norm to 1
+	public void normalize() {
+		Double norm = norm();
+
+		// check if the norm is zero or already 1
+		if(norm == 0.0 || norm == 1.0) {
+			return;
+		}
+
+		for(int i = 0; i < dimension(); i++) {
+			setElement(i, getElement(i) / norm);
+		}
 	}
 }
--- practice-midterm/src/test/java/ch/epfl/sweng/tests/VectorTest.java
+++ practice-midterm/src/test/java/ch/epfl/sweng/tests/VectorTest.java
@@ -15,4 +15,147 @@ public final class VectorTest {
 		Vector v = new Vector(10);
 		assertEquals(10, v.dimension());
 	}
+
+	@Test
+	public void zeroInitTest() {
+		Vector v = new Vector(10);
+		for(int i = 0; i < v.dimension(); i++) {
+			assertEquals(0.0, v.getElement(i), 0.0);
+		}
+	}
+
+	@Test
+	public void updateTest() {
+		Vector v = new Vector(10);
+		for(int i = 0; i < v.dimension(); i++) {
+			v.setElement(i, i);
+		}
+		for(int i = 0; i < v.dimension(); i++) {
+			assertEquals(Double.valueOf(i), v.getElement(i), 0.0);
+		}
+		for(int i = 0; i < v.dimension(); i++) {
+			v.setElement(i, i + 100);
+		}
+		for(int i = 0; i < v.dimension(); i++) {
+			assertEquals(Double.valueOf(i + 100), v.getElement(i), 0.0);
+		}
+	}
+
+	@Test
+	public void normTest() {
+		// empty vector norm is 0
+		Vector v = new Vector(0);
+		assertEquals(0.0, v.norm(), 0.0);
+
+		// norm of [1] * 100 is 10
+		v = new Vector(100);
+		for(int i = 0; i < v.dimension(); i++) {
+			v.setElement(i, 1);
+		}
+		assertEquals(10.0, v.norm(), 0.0);
+	}
+
+	@Test
+	public void dimensionTestZero() {
+		Vector v = new Vector(0);
+		assertEquals(0, v.dimension());
+	}
+
+	@Test
+	public void dimensionTestLessZero() {
+		try {
+			Vector v = new Vector(-1);
+			assertEquals(0, 1);
+		}
+		catch(IllegalArgumentException e) {
+			assertEquals(0, 0);
+		}
+	}
+
+	@Test
+	public void setOutside() {
+		try {
+			Vector v = new Vector(10);
+			v.setElement(10, 0);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+
+		try {
+			Vector v = new Vector(10);
+			v.setElement(-1, 0);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+
+		try {
+			Vector v = new Vector(10);
+			v.setElement(11, 0);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+
+		try {
+			Vector v = new Vector(0);
+			v.setElement(0, 0);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+	}
+
+	@Test
+	public void getOutside() {
+		try {
+			Vector v = new Vector(10);
+			v.getElement(10);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+
+		try {
+			Vector v = new Vector(10);
+			v.getElement(-1);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+
+		try {
+			Vector v = new Vector(10);
+			v.getElement(11);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+
+		try {
+			Vector v = new Vector(0);
+			v.getElement(0);
+			assertEquals(0, 1);
+		}
+		catch(IndexOutOfBoundsException e) {
+			assertEquals(0, 0);
+		}
+	}
+
+	@Test
+	public void stressTest() {
+		final int N = 100000000;
+		Vector v = new Vector(N);
+		for(int i = 0; i < v.dimension(); i++) {
+			assertEquals(0.0, v.getElement(i), 0.0);
+		}
+	}
 }
